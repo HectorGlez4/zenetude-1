30dfd3e7e284bb8d325bfb144ebe55fc86f9fb0c
sknz <florandara@gmail.com>
2014-12-05 03:55:48 +0100

    Git ignore

---
+++ main.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm (tu veux une médaille ?).
+ * User: sknz
+ * Date: 05/12/14
+ * Time: 03:39
+ */
+
+require 'vendor/autoload.php';
+
+$app = new \Slim\Slim(array(
+    'debug' => true
+));
+
+$app->get('/hello/:name', function ($name) {
+    echo 'SWAG YOLO' . $name;
+});
+
+$app->run();
