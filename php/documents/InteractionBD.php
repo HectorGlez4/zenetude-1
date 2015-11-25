9675659bcb72eb12a879c374d69be05d3f3851c8
LoicVigne <loicvigne@gmail.com>
2015-11-25 16:44:11 +0100

    La génération de la feuille utilise la BD

--- php/documents/InteractionBD.php
+++ php/documents/InteractionBD.php
@@ -1,22 +1,47 @@
 <?php
 
-$pdo = new PDO('mysql:host= mysql-maquetteprojet.alwaysdata.net;dbname=maquetteprojet_zenetude', '114038_equipe1', '123456');
+
 
 /**
 * @Params int training_id, int student_group
 * fonction qui récupère les étudiants correspondant au groupe voulu et renvoie cette liste sous form de tableau indexé
 **/
-function getStudentsByTrainingGroup(int $training_id = 0, int $student_group = 0){
+function getStudentsByTrainingGroup($training_id = null, $student_group = null){
 
-    if($training_id == 0 || $student_group == 0)
+    if($training_id == null || $student_group == null)
         return false;
-        
-    $sql = "SELECT  student_id, Student.user_id, student_personnalemail, student_origin, student_avatar, user_name, user_firstname 
-            FROM    Student LEFT JOIN User USING user_id 
-            WHERE   training_id = ".$training_id." 
-            AND     student_group = ".$student_group;
-    $stmt = $pdo->prepare($sql);
-    $stmt->execute();
+      
+    $mysqli = new mysqli("mysql-maquetteprojet.alwaysdata.net", "114038_equipe1", "123456", "maquetteprojet_zenetude");
+    if ($mysqli->connect_errno)
+    {
+        echo "Echec lors de la connexion à MySQL : (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    }
     
-    return $stmt->fetchAll();
+    if ($result = $mysqli->query("SELECT  student_id, Student.user_id, student_personnalemail, student_origin, student_avatar, user_name, user_firstname 
+                                FROM    Student JOIN User ON Student.user_id = User.user_id 
+                                WHERE   training_id = ".$training_id." 
+                                AND     student_group = ".$student_group." 
+                                ORDER BY user_name")) {
+            
+        for ($tablearesultat = array (); $row = $result->fetch_assoc(); $tablearesultat[] = $row);
+        
+        return $tablearesultat;
+    }
+       
+     /*  $dbh = new PDO('mysql:host=mysql-maquetteprojet.alwaysdata.net;dbname=maquetteprojet_zenetude', '114038_equipe1', '123456');  
+       $query = $dbh->prepare("SELECT  student_id, Student.user_id, student_personnalemail, student_origin, student_avatar, user_name, user_firstname 
+                               FROM    Student JOIN User ON Student.user_id = User.user_id 
+                               WHERE   training_id = '".$training_id."' 
+                               AND     student_group = '".$student_group."'");
+                               
+       $query->execute();
+       
+       
+       
+    $sql = $pdo->query("SELECT  student_id, Student.user_id, student_personnalemail, student_origin, student_avatar, user_name, user_firstname 
+                               FROM    Student JOIN User ON Student.user_id = User.user_id 
+                               WHERE   training_id = '".$training_id."' 
+                               AND     student_group = '".$student_group."'");
+    $sql->setFetchMode(PDO::FETCH_BOTH); // Mode par défaut (tableau)
+    return $sql->fetch();*/
 }
