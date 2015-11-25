5586e03b6f166c542b549f2d13a007c0ae75e1ec
Marvyn <marvyn.duvauchelle@gmail.com>
2015-11-25 14:03:50 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm (tu veux une médaille ?).
- * User: sknz
- * Date: 05/12/14
- * Time: 03:39
- */
 
 error_reporting(E_ALL);
 require 'vendor/autoload.php';
@@ -25,41 +19,59 @@ $app->get('/', function () {
     <?php
 });
 
-
-// ITS GONNA MAKE ME CRY
+date_default_timezone_set('Europe/Paris');
 $app->get('/reminder', function () use ($app) {
     $ch = curl_init();
-//    $day =
-    $source = 'http://planning.univ-amu.fr/ade/custom/modules/plannings/anonymous_cal.jsp?resources=6445&projectId=8&startDay=' . date('d', time() + 3 * 24 * 60 * 60) . '&startMonth=' . date('m', time() + 3 * 24 * 60 * 60) . '&startYear=' . date('Y', time() + 3 * 24 * 60 * 60) . '&endDay=' . date('d', time() + 3 * 24 * 60 * 60) . '&endMonth=' . date('m', time() + 3 * 24 * 60 * 60) . '&endYear=' . date('Y', time() + 3 * 24 * 60 * 60) . '';
+    $nbDays = 3;
+    $source = 'http://ade-consult.pp.univ-amu.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?resources=6445&projectId=8&startDay=' . date('d', time() + $nbDays * 24 * 60 * 60 /*+14 jours*/ ) . '&startMonth=' . date('m', time() + $nbDays * 24 * 60 * 60) . '&startYear=' . date('Y', time() + $nbDays * 24 * 60 * 60) . '&endDay=' . date('d', time() + $nbDays * 24 * 60 * 60) . '&endMonth=' . date('m', time() + $nbDays * 24 * 60 * 60) . '&endYear=' . date('Y', time() + $nbDays * 24 * 60 * 60) . '';
     curl_setopt($ch, CURLOPT_URL, $source);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     $data = curl_exec ($ch);
     curl_close ($ch);
 
-    echo '<br/>' . $data . '</br>'.'</br>';
-
     $lines = explode(PHP_EOL, $data);
-    $app->response->write(count($lines) . '<br/>');
-    for ($i = 5; $i < count($lines); ++$i) // i = 6 Pereyti stroki zagolovka
+    for ($i = 5; $i < count($lines); ++$i)
     {
 
-//        echo $i . ' ' . $lines[$i] . '<br/>';
-        if (strstr($lines[$i], "BEGIN:VEVENT", $i))
-        {
-            ++$i; // Skip useless timestamp
-            $date = substr($lines[++$i], 8, 8); //extract day from timestamp
-            ++$i; // Skip useless timestamp
-            echo $lines[++$i] . '<br/>';
-            echo $lines[++$i] . '<br/>';
+         if (strstr($lines[$i], "BEGIN:VEVENT"))
+         {
             ++$i;
-            ++$i;
-            echo $lines[++$i] . '<br />';
-            $app->response->write($date . '<br/>');
+            ++$i;                                                                                               // DATE HEURE MINUTE DEBUT DE COURS
+
+            $date = substr($lines[$i], 8, 8);
+            $hour_start = substr($lines[$i], 17, 2);
+            $min_start = substr($lines[$i], 19, 2);
             $d = date_create_from_format('Ymd', $date);
-            echo $d->format('d/m/Y') . '<br/>' . '<br/>';
-        }
+            echo $d->format('d/m/Y') . ' ' . $hour_start . ':' . $min_start . "</br>";
+            ++$i;                                                                                               // HEURE ET MINUTE FIN DE COURS
+
+            $hour_end = substr($lines[$i], 15, 2);
+            $min_end = substr($lines[$i], 17, 2);
+            echo $d->format('d/m/Y') . ' ' . $hour_end . ':' . $min_end . "</br>";
+
+            ++$i;                                                                                                // MATIERE
+            $topic = substr($lines[$i], 8);
+            echo utf8_decode($topic) . "</br>";
+
+            ++$i;                                                                                               //CLASSE
+            $room = substr($lines[$i], 9);
+            echo $room . "</br>";
+
+            
+            ++$i;
+                                                                                                        //NOM PRENOM
+            $full_description  = $lines[$i]; 
+            $nom_prenom = explode('\n', $full_description);
+            $parameter = utf8_decode($nom_prenom[2]) . "</br>";
+            $first_carac = substr($nom_prenom[2], 0, 1);
+             if ($first_carac == "(") {
+                echo '';
+             }
+             else {
+                echo $parameter;
+             }
+         }
     }
 });
 
 $app->run();
-
