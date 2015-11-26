eec7918d01999b423c6db2cb1d433e72632f9bf9
Marvyn <marvyn.duvauchelle@gmail.com>
2015-11-26 11:12:23 +0100

    Ajout fonction sendReminder

--- ADE_data.php
+++ ADE_data.php
@@ -2,12 +2,81 @@
 
     error_reporting(E_ALL);
     require 'vendor/autoload.php';
+    require 'PHPMailer/PHPMailerAutoload.php';
 
     function toJSON($app, $content) {
         $response = $app->response;
         $response['Content-Type'] = 'application/json';
         $response->body( json_encode($content) );
     };
+
+
+
+        
+
+
+      
+    // $mysqli = new mysqli("mysql-maquetteprojet.alwaysdata.net", "114038_equipe1", "123456", "maquetteprojet_zenetude");
+    //     if ($mysqli->connect_errno)
+    //     {
+    //         echo "Echec lors de la connexion à MySQL : (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    //     }
+    
+    //     if ($result = $mysqli->query("SELECT user_instituteemail FROM user")) {
+            
+    //     for ($tablearesultat = array (); $row = $result->fetch_assoc(); $tablearesultat[] = $row);
+        
+    //     echo $tablearesultat;
+    
+//}
+
+    function sendReminder($first_name, $last_name) {
+                    //Create a new PHPMailer instance
+                $mail = new PHPMailer;
+
+                //Set who the message is to be sent from
+                $mail->setFrom('Zenetude', 'First Last');
+
+                //Set an alternative reply-to address
+                /*$mail->addReplyTo('replyto@example.com', 'First Last');*/
+
+
+
+                $db = new PDO('mysql:host=localhost; dbname=maquetteprojet_zenetude', 'root', 'root');         
+            
+                $request = $db -> prepare('SELECT user_instituteemail FROM user WHERE "' . $first_name . '"=user_firstname AND "' . $last_name . '"=user_name');
+                $request -> execute();
+                $results = $request -> fetchAll();
+                echo $first_name , $last_name;
+                foreach ($results as $result) {
+
+                    $mailbd = $result[0];
+                    echo $mailbd;
+                //Set who the message is to be sent to
+                $mail->addAddress($mailbd, $first_name . ' ' . $last_name);
+
+                //Set the subject line
+                $mail->Subject = 'PHPMailer mail() test';
+
+                //Read an HTML message body from an external file, convert referenced images to embedded,
+                //convert HTML into a basic plain-text alternative body
+                $mail->isHTML(true);
+
+                //Replace the plain text body with one created manually
+                //$mail->Body = file_get_contents('examples/contents.html');
+                $mail->Body = "TEST DE BG MEK";
+                //Attach an image file
+                //$mail->addAttachment('examples/images/phpmailer_mini.png');
+
+                //send the message, check for errors
+
+                if (!$mail->send()) {
+                    echo "Mailer Error: " . $mail->ErrorInfo;
+                } else {
+                    echo "Message sent!";
+                }
+            }
+        }
 
     $app = new \Slim\Slim(array(
         'debug' => true
@@ -27,15 +96,14 @@
         $ch = curl_init();
 
         //Number of day the data come from
-        $nbDays = 3;
-
+        $nbDays = 12;
         //Agenda's URL where data come from
         $source = 'http://ade-consult.pp.univ-amu.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?resources=6445&projectId=8&startDay=' . date('d', time() + $nbDays * 24 * 60 * 60 /*+14 jours*/ ) . '&startMonth=' . date('m', time() + $nbDays * 24 * 60 * 60) . '&startYear=' . date('Y', time() + $nbDays * 24 * 60 * 60) . '&endDay=' . date('d', time() + $nbDays * 24 * 60 * 60) . '&endMonth=' . date('m', time() + $nbDays * 24 * 60 * 60) . '&endYear=' . date('Y', time() + $nbDays * 24 * 60 * 60) . '';
         curl_setopt($ch, CURLOPT_URL, $source);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         $data = curl_exec ($ch);
         curl_close ($ch);
-
+        //echo $data;
         $lines = explode(PHP_EOL, $data);
 
         for ($i = 5; $i < count($lines); ++$i)
@@ -89,10 +157,13 @@
                 else {
                     echo $parameter ."</br>";
                     $full_name = explode(' ', $parameter);
-                    $last_name = strtolower($full_name[0]) . "</br>";
-                    $first_name = strtolower($full_name[1]) . "</br>";
+                    // $last_name = strtolower($full_name[0]) . "</br>";
+                    // $first_name = strtolower($full_name[1]) . "</br>";
+                    echo 'yolo';
+                    sendReminder('dylan', 'prudhomme');
+
                 }
-            }
+           }
         }
     });
 
