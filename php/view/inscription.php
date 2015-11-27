088e902ec464d48a0401d10f2072e2b7a6f5879a
Kenny Aime <aime.kenny@gmail.com>
2015-11-27 15:56:34 +0100

    Visuel inscription

--- php/view/inscription.php
+++ php/view/inscription.php
@@ -2,53 +2,84 @@
 <html lang="fr">
 <head>
     <title>Zenetude</title>
-	<link rel="stylesheet" href="../css/bootstrap.css">
-    <link rel="stylesheet" href="../css/index.css">
-    <link rel="stylesheet" href="../css/test.css">
-
+    <link rel="stylesheet" href="../../css/materialize.css">
+    <link rel="stylesheet" href="../../css/index.css">
+    <link rel="stylesheet" href="../../css/style.css">
     <meta charset="utf-8">
 </head>
 <body>
-<?php
-	
-?>
-<div class="container-fluid">
+    <!-- CONTAINER -->
+    <div class="container-fluid">
 
-	<div class="nav row">
-        <div class='col-md-3 logo'>
-        <a href='../web/index.html'>
-        <img src='../img/logo.jpg' alt='Logo'/>
-        </a>
-		</div>
-    <div class='col-md-9 titre'>
-       <h1>Bannière ZENETUDE</h1>
-    </div>
-    </div>
-	<div class='col-md-8 content'>
-
-
-            <form id="inscription" action ="valider.php" method="post" onsubmit="">
-                <fieldset>
-                    <legend><h2>Inscription</h2></legend>
-                <label for="mail"> Adresse email <em>*</em></label>
-                <input id="email" type="email" name="mail"  ><br>
-                <label for="passe">Mot de passe: <em>*</em> </label>
-                <input id ="passe" type="password"  name="passe"/><br/>
-                <label for="passe2">Confirmation du mot de passe: </label>
-                <input id="passe" type="password"  name="passe2"/><br/>
-                <input value="S'inscrire" type="submit">
-                </fieldset>
+    <!--<div id="content">
+        <h2>Inscription</h2>
+        <div class='renseignements'>
+            <form action ="valider.php" method="post">
+                <label>Adresse e-mail: <input type="text" id ="email" name="EMAIL"/></label><br/>
+                <label>Mot de passe: <input type="password" id ="passe" name="PASSE"/></label><br/>
+                <label>Confirmation du mot de passe: <input type="password" id="passe" name="PASSE2"/></label><br/>
+                <input type="submit" value="M'inscrire"/>
             </form>
 
 
-	</div>
-		<div id="footer">
-        <p>
-			ZENETUDE - Projet réalisé par les étudiants de LP SIL DA2I 2015/2016
-		</p>
 
-		</div>
-		
-</div>		
+        </div>
+    </div>-->
+
+    <div class="row">
+      <!-- Debut card -->
+      <div class="card-panel inscription col m4 push-m4 s12 center-align">
+        <!-- Formulaire -->
+        <form class="col formulaire s10 push-s1" action="valider.php" method="POST" onsubmit="">
+
+          <!-- Titre de la carte -->
+          <div class="card-header"> <h2>Inscription</h2></div>
+          <!-- Fin titre -->
+
+          <!-- Contenu card -->
+          <div class="card-content">
+
+          <!-- email -->
+          <div class="row">
+            <div class="input-field col s12">
+              <input id="email" type="email" class="validate" name="mail">
+              <label for="email">Adresse email <em>*</em></label>
+            </div>
+          </div><!-- fin email -->
+
+          <!-- mot de passe -->
+          <div class="row">
+            <div class="input-field col s12">
+              <input id="passe" type="password" class="validate" name="passe">
+              <label for="passe">Mot de passe <em>*</em></label>
+            </div>
+          </div><!-- fin mot de passe -->
+
+          <!-- confirmation mot de passe -->
+          <div class="row">
+            <div class="input-field col s12">
+              <input id="passe2" type="password" class="validate" name="passe2">
+              <label for="passe2">Confirmer votre mot de passe</label>
+            </div>
+          </div><!-- fin confirmation mot de passe -->
+
+        </div><!-- Fin contenu card -->
+
+          <!-- bouton s'inscrire -->
+          <div class="card-action bouton-connection">  
+            <input class="btn center-align" type="submit" value="S'inscrire" />
+          </div>
+
+        </form><!-- Fin formulaire -->
+      </div><!-- Fin card -->
+  </div>
+
+    
+</div><!-- FIN CONTAINER -->
+    <!--<div class="footer">
+        <p> ZENETUDE - Projet réalisé par les étudiants de LP SIL DA2I 2015/2016 </p>
+    </div> -->
+    <script type="text/javascript" src="../../js/jquery-1.11.3.min.js"></script>
+    <script type="text/javascript" src="../../js/materialize.js"></script>
 </body>
 </html>
