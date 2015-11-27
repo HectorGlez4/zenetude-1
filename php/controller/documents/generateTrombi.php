8274975e65f5cf74331d40d6528c31ed469d539a
Hector Gonzalez <hector.glez4@gmail.com>
2015-11-27 11:44:50 +0100

    trombi

---
+++ php/controller/documents/generateTrombi.php
@@ -0,0 +1,21 @@
+<?php
+    // get the HTML
+    ob_start();
+    include(dirname(__FILE__).'/trombiTemplate.php');
+
+    $content = ob_get_clean();
+
+    // convert to PDF
+    require_once('./html2pdf.class.php');
+    try
+    {
+        $html2pdf = new HTML2PDF('P', 'A4', 'fr');
+        $html2pdf->pdf->SetDisplayMode('fullpage');
+//      $html2pdf->pdf->SetProtection(array('print'), 'spipu');
+        $html2pdf->writeHTML($content, isset($_GET['vuehtml']));
+        $html2pdf->Output('trombi.pdf');
+    }
+    catch(HTML2PDF_exception $e) {
+        echo $e;
+        exit;
+    }
