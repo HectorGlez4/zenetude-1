3a6d5e89b3c9416721b5ba0f1165e5d301b3cbb6
Nordine M'LISSA <nordine.mlissa@gmail.com>
2015-11-27 18:41:30 +0100

    debug

--- php/view/valider.php
+++ php/view/valider.php
@@ -1,8 +1,8 @@
 <?php
-    include_once( '../php/model/db.php');
-    include_once('../php/controller/AccountController.php');
-    include_once('../php/model/AccountModel.php');
-    include_once('../php/view/AccountView.php');
+    include_once( '../model/db.php');
+    include_once('../controller/AccountController.php');
+    include_once('../model/AccountModel.php');
+    include_once('AccountView.php');
     ini_set('display_errors', 1);
     $accountController = new AccountController();
     $accountController->controlInscription();
