3dd63192895dd1302eb0e0afbfefcf11a9f68d96
Cédric Vigne <cedric.vigne11@gmail.com>
2015-11-29 12:51:18 +0100

    Nouveaux fichiers mvc

---
+++ php/controller/PageController.php
@@ -0,0 +1,35 @@
+<?php
+	class PageController {
+		public function controlHeader() {
+			$pageView = new PageView();
+			if(isset($_SESSION['nom']))
+				$pageView -> showHeader(true);
+			else
+				$pageView -> showHeader(false);
+		}
+
+		public function controlMenu() {
+			$pageView = new PageView();
+			if(isset($_SESSION['nom'])) {
+				$_SESSION['nom'] = htmlspecialchars($_SESSION['nom']);
+				$_SESSION['prenom'] = htmlspecialchars($_SESSION['prenom']);
+				$_SESSION['class'] = htmlspecialchars($_SESSION['class']);
+				$pageView -> showMenu(true);
+			}
+				
+			else 
+				$pageView -> showMenu(false);
+		}
+
+		public function controlScrollMenu() {
+			$pageView = new PageView();
+			if(isset($_SESSION['nom'])) {
+				$_SESSION['nom'] = htmlspecialchars($_SESSION['nom']);
+				$_SESSION['prenom'] = htmlspecialchars($_SESSION['prenom']);
+				$_SESSION['class'] = htmlspecialchars($_SESSION['class']);
+				$pageView -> showScrollMenu(true);
+			}	
+			else 
+				$pageView -> showScrollMenu(false);
+		}
+	}
