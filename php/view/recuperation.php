40a53ae5c4d29399bcda8e494414795551c762b3
ThaStupidKid <kevin.simon.r7@gmail.com>
2015-11-29 16:08:56 +0100

    modification de l'interface de la page de récupération de mdp

--- php/view/recuperation.php
+++ php/view/recuperation.php
@@ -1,51 +1,81 @@
 <!doctype html>
 <html lang="fr">
-    <head>
-        <title>ZenEtude</title>
-        <link rel="stylesheet" href="../css/bootstrap.css">
-        <link rel="stylesheet" href="../css/index.css">
+<head>
+    <title>Zenetude</title>
+    <link rel="stylesheet" href="../../css/materialize.css">
+    <link rel="stylesheet" href="../../css/index.css">
+    <link rel="stylesheet" href="../../css/style.css">
+    <meta charset="utf-8">
+</head>
+<body>
+    <!-- CONTAINER -->
+    <div class="container-fluid">
 
-        <meta charset="utf-8">
-    </head>
-    <body>
-        <div class="container-fluid">
+    <!--<div id="content">
+        <h2>Inscription</h2>
+        <div class='renseignements'>
+            <form action ="valider.php" method="post">
+                <label>Adresse e-mail: <input type="text" id ="email" name="EMAIL"/></label><br/>
+                <label>Mot de passe: <input type="password" id ="passe" name="PASSE"/></label><br/>
+                <label>Confirmation du mot de passe: <input type="password" id="passe" name="PASSE2"/></label><br/>
+                <input type="submit" value="M'inscrire"/>
+            </form>
 
-            <div class="nav row">
-                <div class='col-md-3 logo'>
-                   <a href='../index.html'>
-                    <img src='../img/logo.jpg' alt='Logo'/>
-                    </a>
-                </div>
-                <div class='col-md-9 titre'>
-                    <h1>Bannière ZENETUDE</h1>
-                </div>
-            </div>
 
-            <div class='col-md-8 content'>
-           	<form method="post" action="./recuperation.php">
-			<label for="mail">Entrez votre adresse mail : </label><input type="email" id="mail" name="mail" />
-			<input type="submit" name="submit" value ="Envoyer" />
-		</form> 
-	   	<?php
-			ini_set('display_errors', '1');	
-			if(isset($_POST['mail'])) {
-				include_once('../php/controller/AccountController.php');
-				include_once('../php/model/AccountModel.php');
-				include_once('../php/view/AccountView.php');
-				include_once('../PHPMailer/PHPMailerAutoload.php');
-                include_once( '../php/model/db.php');
-				$accountController = new AccountController();
-				$accountController -> controlRecoverPassword();
-			}
-		?>
-	   </div>
-            <div class='footer'>
-                <p>
-                    ZENETUDE - Projet réalisé par les étudiants de LP SIL DA2I 2015/2016
-                </p>
 
-            </div>
         </div>
-    </body>
-</html>
+    </div>-->
 
+    <div class="row">
+      <!-- Debut card -->
+      <div class="card-panel inscription col m4 push-m4 s12 center-align">
+        <!-- Formulaire -->
+        <form class="col formulaire s10 push-s1" action="./recuperation.php" method="POST" onsubmit="">
+
+          <!-- Titre de la carte -->
+          <div class="card-header"> <h2>Inscription</h2></div>
+          <!-- Fin titre -->
+
+          <!-- Contenu card -->
+          <div class="card-content">
+
+          <!-- email -->
+          <div class="row">
+            <div class="input-field col s12">
+              <input id="email" type="email" class="validate" name="mail">
+              <label for="email">Entrer l'adresse email de récupération </label>
+            </div>
+          </div><!-- fin email -->
+
+        </div><!-- Fin contenu card -->
+
+          <!-- bouton s'inscrire -->
+          <div class="card-action bouton-connection">  
+            <input class="btn center-align" type="submit" value="Valider" />
+          </div>
+
+        </form><!-- Fin formulaire -->
+        <?php
+            ini_set('display_errors', '1'); 
+            if(isset($_POST['mail'])) {
+                include_once('../php/controller/AccountController.php');
+                include_once('../php/model/AccountModel.php');
+                include_once('../php/view/AccountView.php');
+                include_once('../PHPMailer/PHPMailerAutoload.php');
+                include_once( '../php/model/db.php');
+                $accountController = new AccountController();
+                $accountController -> controlRecoverPassword();
+            }
+        ?>
+      </div><!-- Fin card -->
+  </div>
+
+    
+</div><!-- FIN CONTAINER -->
+    <!--<div class="footer">
+        <p> ZENETUDE - Projet réalisé par les étudiants de LP SIL DA2I 2015/2016 </p>
+    </div> -->
+    <script type="text/javascript" src="../../js/jquery-1.11.3.min.js"></script>
+    <script type="text/javascript" src="../../js/materialize.js"></script>
+</body>
+</html>
