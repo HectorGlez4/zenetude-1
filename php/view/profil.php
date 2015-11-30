4b6ba61eea74769d1386497850d4b22f66086069
darkweizer <darkweizer@gmail.com>
2015-11-30 01:49:16 +0100

    modif lien include recuperation, refonte de la page profil en fonction du profil connecté

--- php/view/profil.php
+++ php/view/profil.php
@@ -5,9 +5,16 @@
     }
     include_once('./PageView.php');
     include_once('../controller/PageController.php');
+    include_once('../model/db.php');
 
     $pageController = new PageController();
     $pageView = new PageView();
+    $db = connect();
+
+    if (isset($_SESSION['id'])) {       
+        
+    }
+
 ?>
   <!DOCTYPE html>
   <html>
@@ -27,28 +34,9 @@
                 <div class="col s12 m8">
                 <div class="card-panel teal" id="bloc2">
                     <div class="card-title"> <h3>Profil</h3></div>
-                    <ul>
-                        <li class="infos">Sexe :
-                            <input class="with-gap" name="sexe" type="radio" id="h" value="1"/>
-                            <label for="h">Homme</label>
-                        
-                            <input class="with-gap" name="sexe" type="radio" id="f" value="2"/>
-                            <label for="f">Femme</label>
-                        </li>
-                        <li class="infos">Formation : ...</li>
-                        <li class="infos">Classe : </li>
-                        <li class="infos">Date de naissance : </li>
-                        <li class="infos">Lieu de naissance : </li>
-                        <li class="infos">Mail : </li>
-                        <li class="infos">Téléphone : </li>
-                        <li class="infos">Adresse personnelle : </li>
-                        <li class="infos">
-                            <a href="contact.php">Contacter un responsable de formation</a>
-                        </li>
-                        <li class="infos">
-                            <a class="right-align" href="trombi.php">Documents pédagogiques</a>
-                        </li>
-                    </ul>
+                    <?php
+                        $pageView -> showInformations($db);
+                    ?>
                     <p>
                     </p>
                 </div>
