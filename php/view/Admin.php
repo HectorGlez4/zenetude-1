7ab79335806b1af0e5f081948e3ea0b30d4888d4
PruDyl <dylan.prudhomme13@gmail.com>
2015-11-30 16:59:00 +0100

    Commencement interface Admin

---
+++ php/view/Admin.php
@@ -0,0 +1,44 @@
+<?php
+    session_start();
+    if (isset($_GET['erreur'])){
+        echo "<script>alert('Erreur d\'authentification !');</script>";
+    }
+    include_once('./PageView.php');
+    include_once('../controller/PageController.php');
+    include_once('../model/db.php');
+
+    $pageController = new PageController();
+    $pageView = new PageView();
+    $db = connect();
+
+?>
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+            $pageView -> showHead();
+            $pageController -> controlHeader();
+            $pageController -> controlDynamicMenu();
+        ?>
+        <div class="container">
+            <div class="row">
+                <div class="col s12 m8">
+                    <div class="card-panel teal" id="bloc2">
+                        <div class="card-title"> <h3>Profil</h3></div>
+                        <?php
+                            $pageView -> showAdministration($db);
+                        ?>
+                        <p></p>
+                    </div>
+                </div>
+                <?php
+                    $pageView->showCalendar();
+                ?>
+            </div>
+        </div><!-- Fin container -->
+        <?php
+            $pageView->showFooter();
+            $pageView->showjavaLinks();
+        ?>
+    </body>
+</html>
