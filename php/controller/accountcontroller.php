37fcad7af481dfd5e264ec7f2f26056538692809
PruDyl <dylan.prudhomme13@gmail.com>
2016-01-06 11:22:43 +0100

    Fix sécurité valeur groupe et bac

--- php/controller/accountcontroller.php
+++ php/controller/accountcontroller.php
@@ -27,7 +27,7 @@
                 $accountModel = new AccountModel();
                 $_POST['mail'] = htmlspecialchars($_POST['mail']);
                 $_POST['pass'] = htmlspecialchars(sha1($_POST['pass']));
-                if ($userResult = $accountModel -> getUserByPassword($_POST['mail'],  $_POST['pass'])){
+                if ($userResult = $accountModel -> getUserPassword($_POST['mail'],  $_POST['pass'])){
                     //session_start();
                     $_SESSION['infoUser'] = $userResult;
                  
@@ -38,7 +38,7 @@
                    	else {
                    		$studentResult = $accountModel->getDataStudent('*', $_SESSION['infoUser']['user_id']);
 						$_SESSION['infoStudent'] = $studentResult;
-                        $trainingResult = $accountModel->getTrainingInformationsForUser('description', $_SESSION['infoUser']['user_id']);
+                        $trainingResult = $accountModel->getTrainingInformationsForUser('*', $_SESSION['infoUser']['user_id']);
                         $_SESSION['infoTraining'] = $trainingResult;
                    	}
                     $accountView -> showMessage(null,"ok","index.php");
